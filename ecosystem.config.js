4602f0db28281cc2e19af58ead120024da92ac7f
a <a@a.r>
2022-10-16 11:31:16 +0500

    new production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,9 +8,19 @@ module.exports = {
   ],
 
   deploy : {
-    production : {
+    production_old: {
       user : 'deploy',
       host : '194.87.99.114',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Vladislavbro/goods_hunter.git',
+      path : '/home/deploy/apps/goods_hunter',
+      // 'pre-deploy-local': '',
+      // 'post-deploy' : 'cd frontend && npm install && npm run build && cd .. && pm2 reload ecosystem.config.js --env production',
+      // 'pre-setup': ''
+    },
+    production : {
+      user : 'deploy',
+      host : '109.196.164.236',
       ref  : 'origin/master',
       repo : 'git@github.com:Vladislavbro/goods_hunter.git',
       path : '/home/deploy/apps/goods_hunter',
