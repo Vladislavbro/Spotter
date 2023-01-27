35020738fac25230b95b5aefca65b4960fef2cbc
a <a@a.r>
2023-01-27 15:30:11 +0500

    xz

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -5,24 +5,19 @@
       <input class="tinkoffPayRow" type="hidden" name="terminalkey" value="1664189383150DEMO">
       <input class="tinkoffPayRow" type="hidden" name="frame" value="true">
       <input class="tinkoffPayRow" type="hidden" name="language" value="ru">
-        <input class="tinkoffPayRow" type="text" placeholder="Сумма заказа" name="amount" required>
-        <input class="tinkoffPayRow" type="text" placeholder="Номер заказа" name="order">
-        <input class="tinkoffPayRow" type="text" placeholder="Описание заказа" name="description">
-        <input class="tinkoffPayRow" type="text" placeholder="ФИО плательщика" name="name">
-        <input class="tinkoffPayRow" type="text" placeholder="E-mail" name="email">
-        <input class="tinkoffPayRow" type="text" placeholder="Контактный телефон" name="phone">
-        <input class="tinkoffPayRow" type="submit" value="Оплатить">
+      <input class="tinkoffPayRow" type="text" placeholder="Сумма заказа" name="amount" required>
+      <input class="tinkoffPayRow" type="text" placeholder="Номер заказа" name="order">
+      <input class="tinkoffPayRow" type="text" placeholder="Описание заказа" name="description">
+      <input class="tinkoffPayRow" type="text" placeholder="ФИО плательщика" name="name">
+      <input class="tinkoffPayRow" type="text" placeholder="E-mail" name="email">
+      <input class="tinkoffPayRow" type="text" placeholder="Контактный телефон" name="phone">
+      <input class="tinkoffPayRow" type="submit" value="Оплатить">
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  // mounted() {
-  //   let tinkoffScript = document.createElement('script')
-  //   tinkoffScript.setAttribute('src', 'https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js')
-  //   document.head.appendChild(tinkoffScript)
-  // },
 }
 </script>
 
