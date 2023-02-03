0743b3082683ae4a0153f16d7e6abb9fa20ef862
a <a@a.r>
2023-02-03 19:04:14 +0500

    fix

--- parse.py
+++ parse.py
@@ -691,14 +691,14 @@ class Parser(object):
         query = Queries.objects(
             current_parsing_id=self.config.current_parsing_id,
             calculated__ne=True,
-            products_count__ne=None
+            products_count__gte=1,
         ).first()
         while query:
             self.calculate_query(query)
             query = Queries.objects(
                 current_parsing_id=self.config.current_parsing_id,
                 calculated__ne=True,
-                products_count__ne=None,
+                products_count__gte=1,
             ).first()
         self.config.queries_calculated = True
         self.config.save()
